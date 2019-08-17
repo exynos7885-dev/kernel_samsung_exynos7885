baee611948fd3387a6e8966234064b8de0fcadff
LuK1337 <priv.luk@gmail.com>
2019-08-18 00:54:54 +0200

    input: sec_ts: Use KEY_WAKEUP for DT2W
    Change-Id: I86b384a07c425d1274a435ee6dff1f68c26596c9

--- drivers/input/touchscreen/sec_ts/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/sec_ts.c
@@ -1299,7 +1299,7 @@ static void sec_ts_read_event(struct sec_ts_data *ts)
 				input_info(true, &ts->client->dev, "%s: AOD: %d, %d, %d\n",
 								__func__, ts->scrub_id, ts->scrub_x, ts->scrub_y);
 #endif
-				input_report_key(ts->input_dev, KEY_BLACK_UI_GESTURE, 1);
+				input_report_key(ts->input_dev, KEY_WAKEUP, 1);
 				ts->all_aod_tap_count++;
 				break;
 			case SEC_TS_GESTURE_CODE_SINGLE_TAP:
@@ -1364,6 +1364,7 @@ static void sec_ts_read_event(struct sec_ts_data *ts)
 
 			input_sync(ts->input_dev);
 			input_report_key(ts->input_dev, KEY_BLACK_UI_GESTURE, 0);
+			input_report_key(ts->input_dev, KEY_WAKEUP, 0);
 			break;
 
 		default:
@@ -2026,6 +2027,7 @@ static void sec_ts_set_input_prop(struct sec_ts_data *ts, struct input_dev *dev,
 	set_bit(BTN_TOUCH, dev->keybit);
 	set_bit(BTN_TOOL_FINGER, dev->keybit);
 	set_bit(KEY_BLACK_UI_GESTURE, dev->keybit);
+	set_bit(KEY_WAKEUP, dev->keybit);
 #ifdef SEC_TS_SUPPORT_TOUCH_KEY
 	if (ts->plat_data->support_mskey) {
 		int i;
